41f00e52a3900cd1e5b78150f8f123c969cf839d
gnahidi <nahidi_ghazaleh@bah.com>
2014-10-13 20:52:40 -0400

    Homework #3

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,14 +1,57 @@
 class WinesController < ApplicationController
+before_action :set_wine, only: [:show, :edit, :update, :destroy]
+
+def set_wine
+	@wine = Wine.find(params[:id])
+end
+
+ #Homework 2
   def index
   	@available_at = Time.now
   	@wines = Wine.all
   end
 
+ #Homework 2
   def show
-  	@wine = Wine.find(params[:id])
+  	
   end
 
+#Homework 3
   def edit
-  	@wine = Wine.find(params[:id])
+  	
   end
+
+#Homework 3
+ def update
+	@wine = Wine.find(params[:id])
+	@wine.update(wine_params)
+	redirect_to @wine
+ end
+
+#Homework 3
+def destroy
+	@wine.destroy
+	redirect_to wines_url
+end
+
+#Homework 3
+def new
+	@wine = Wine.new
+end
+
+#Homework 3
+def create
+	@wine = Wine.new(wine_params)
+	if @wine.save
+		redirect_to @wine, notice: "#{wine.name} was created!"
+	else
+		render :new
+	end
+end
+
+
+def wine_params
+	params.require(:wine).permit(:name, :winery, :country, :year)
+end
+
 end
